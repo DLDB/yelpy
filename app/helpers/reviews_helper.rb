9d60d55b7e40355ab52255fdf879c1b83c62d2c4
shelleyhope <shelleyandkenny@mac.com>
2014-05-22 13:31:03 +0100

    stars with passing tests

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,11 +1,11 @@
 module ReviewsHelper
 
 	def star_rating(number)
+		return number if number == 'N/A'
 	  number = number.round
 	  filled_stars = '★' * number 
     empty_stars = '☆' * (5 - number)
 		filled_stars + empty_stars
-
 	end
 
 end
